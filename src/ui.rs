db20995a45ee876cfc31bbc2ddc39284541ae191
binhminhanh1235 <binhminhanh1235@gmail.com>
2026-09-03 17:03:04 +0700

    feat: add embedded local UI assets

---
+++ src/ui.rs
@@ -0,0 +1,33 @@
+use axum::{
+    body::Body,
+    http::{header::CONTENT_TYPE, HeaderValue, Response, StatusCode},
+    response::Html,
+};
+
+const INDEX_HTML: &str = include_str!("../ui/index.html");
+const APP_CSS: &str = include_str!("../ui/app.css");
+const APP_JS: &str = include_str!("../ui/app.js");
+
+pub async fn index() -> Html<&'static str> {
+    Html(INDEX_HTML)
+}
+
+pub async fn app_css() -> Response<Body> {
+    asset_response("text/css; charset=utf-8", APP_CSS)
+}
+
+pub async fn app_js() -> Response<Body> {
+    asset_response("text/javascript; charset=utf-8", APP_JS)
+}
+
+fn asset_response(content_type: &str, content: &'static str) -> Response<Body> {
+    let mut response = Response::builder()
+        .status(StatusCode::OK)
+        .body(Body::from(content))
+        .expect("valid UI response");
+    response.headers_mut().insert(
+        CONTENT_TYPE,
+        HeaderValue::from_str(content_type).expect("valid UI content type"),
+    );
+    response
+}
